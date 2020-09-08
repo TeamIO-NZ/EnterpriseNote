9fab002ed72f0aa056d08e9cbe64401f5332a405
James <Jameshessell@gmail.com>
2020-09-09 11:58:41 +1200

    feat: working single notes with mux

--- pkg/web/web.go
+++ pkg/web/web.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 //Server this is the struct that contains the webserver information
@@ -14,6 +16,7 @@ type Server struct {
 }
 
 type Note struct {
+	Id      string `json:"Id"`
 	Title   string `json:"Title"`
 	Desc    string `json:"Desc"`
 	Content string `json:"Content"`
@@ -28,8 +31,8 @@ func (server Server) Start() {
 		//zap.S().Warn("No webserver port config detected, using 8080.")
 	}
 	Notes = []Note{
-		Note{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-		Note{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+		Note{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Note{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
 	}
 	server.handleRequests()
 
@@ -43,9 +46,28 @@ func (server Server) returnAllNotes(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(Notes)
 }
 
+//use mux to get us single notes
+func (server Server) returnSingleNote(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	for _, note := range Notes {
+		if note.Id == key {
+			json.NewEncoder(w).Encode(note)
+		}
+	}
+}
+
 //run me to make the server work
 func (server Server) handleRequests() {
-	http.HandleFunc("/", server.homePage)
-	http.HandleFunc("/notes", server.returnAllNotes)
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(server.port), nil))
+	// creates a new instance of a mux router
+	myRouter := mux.NewRouter().StrictSlash(true)
+	// replace http.HandleFunc with myRouter.HandleFunc
+	myRouter.HandleFunc("/", server.homePage)
+	myRouter.HandleFunc("/notes", server.returnAllNotes)
+	myRouter.HandleFunc("/note/{id}", server.returnSingleNote)
+	// finally, instead of passing in nil, we want
+	// to pass in our newly created router as the second
+	// argument
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(server.port), myRouter))
 }
