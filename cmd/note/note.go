f9651cbc6ca6f2871d7ed8f0644a742c5cae5218
James <Jameshessell@gmail.com>
2020-09-09 11:12:46 +1200

    feat: all notes basic endpoint

--- cmd/note/note.go
+++ cmd/note/note.go
@@ -20,7 +20,7 @@ func Start() {
 	zap.S().Info("Logger initialized.")
 	// 3. establish connection to postgresql
 	// 4. start webserver
-	web.WebServer{}.Start()
+	web.Server{}.Start()
 	// 5. wait for kill sig
 	// 6. shutdown webserver
 }
--- pkg/web/web.go
+++ pkg/web/web.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,11 +13,23 @@ type Server struct {
 	port int
 }
 
+type Note struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"Desc"`
+	Content string `json:"Content"`
+}
+
+var Notes []Note
+
 //Start this is the function that starts the webserver
 func (server Server) Start() {
 	if server.port == 0 {
 		server.port = 8080
 		//zap.S().Warn("No webserver port config detected, using 8080.")
+	}
+	Notes = []Note{
+		Note{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Note{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
 	}
 	server.handleRequests()
 
@@ -25,9 +38,14 @@ func (server Server) homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the home page!")
 	fmt.Println("Endpoint Hit: homepage")
 }
+func (server Server) returnAllNotes(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("EndpointHit: return all notes")
+	json.NewEncoder(w).Encode(Notes)
+}
 
 //run me to make the server work
 func (server Server) handleRequests() {
 	http.HandleFunc("/", server.homePage)
+	http.HandleFunc("/notes", server.returnAllNotes)
 	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(server.port), nil))
 }
