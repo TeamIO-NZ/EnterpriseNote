2282c610c488d57dcef4ab4c54fbc9de9bba5fba
James <Jameshessell@gmail.com>
2020-09-09 10:39:19 +1200

    Me breaking things

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"go.iosoftworks.com/EnterpriseNote/cmd"
+)
+
 func main() {
-	//note.Start()
+	cmd.HandleRequests()
 }
--- pkg/web/web.go
+++ pkg/web/web.go
@@ -1,5 +1,11 @@
 package web
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 type WebServer struct {
 	port int
 }
@@ -8,4 +14,16 @@ func (server WebServer) Start() {
 	if server.port == 0 {
 		server.port = 8080
 	}
+	handleRequests()
+
+}
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the home page!")
+	fmt.Println("Endpoint Hit: homepage")
+}
+
+//run me to make the server work
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":10000", nil))
 }
