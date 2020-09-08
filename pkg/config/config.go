4dace6fab5aa6c1c660bb58577fbc1efe5ce2b01
Joe van der Zwet <me@joezwet.me>
2020-09-09 11:01:39 +1200

    feat: initial config setup

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,15 +1,24 @@
 package config
 
 type Config struct {
-	webserver  WebServerConfig  `json:"webserver"`
-	postgresql PostgresqlConfig `json:"postgresql"`
+	WebServer  WebServerConfig  `json:"webserver"`
+	Postgresql PostgresqlConfig `json:"postgresql"`
 }
 
 type WebServerConfig struct {
-	port int `json:"port"`
+	Port string `json:"port"`
 }
 
 type PostgresqlConfig struct {
-	address string `json:"address"`
-	port    int    `json:"port"`
+	Address string `json:"address"`
+	Port    int    `json:"port"`
+}
+
+func (c Config) Init(filename string) (err error) {
+
+	return nil
+}
+
+func (c Config) Validate() (err error) {
+	return nil
 }
--- pkg/web/web.go
+++ pkg/web/web.go
@@ -2,20 +2,20 @@ package web
 
 import (
 	"fmt"
+	"go.iosoftworks.com/EnterpriseNote/pkg/config"
 	"log"
 	"net/http"
-	"strconv"
 )
 
 //Server this is the struct that contains the webserver information
 type Server struct {
-	port int
+	config config.WebServerConfig
 }
 
 //Start this is the function that starts the webserver
 func (server Server) Start() {
-	if server.port == 0 {
-		server.port = 8080
+	if server.config.Port == "" {
+		server.config.Port = "8080"
 		//zap.S().Warn("No webserver port config detected, using 8080.")
 	}
 	server.handleRequests()
@@ -29,5 +29,5 @@ func (server Server) homePage(w http.ResponseWriter, r *http.Request) {
 //run me to make the server work
 func (server Server) handleRequests() {
 	http.HandleFunc("/", server.homePage)
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(server.port), nil))
+	log.Fatal(http.ListenAndServe(":"+server.config.Port, nil))
 }
