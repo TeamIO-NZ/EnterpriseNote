89f1d4f170a006d73e0d04b6e072d7e8b8a4c989
lithial <Jameshessell@gmail.com>
2020-10-18 20:03:19 +1300

    feat: partially broken but maybe working sql code

--- pkg/web/web.go
+++ pkg/web/web.go
@@ -4,10 +4,10 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
@@ -19,6 +19,12 @@ import (
 //Server this is the struct that contains the webserver information
 type Server struct {
 	config config.WebServerConfig
+}
+
+// response format
+type response struct {
+	ID      int64  `json:"id,omitempty"`
+	Message string `json:"message,omitempty"`
 }
 
 //Note a note object for json
@@ -44,6 +50,7 @@ func (server Server) Start() {
 		Note{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
 		Note{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
 	}
+
 	server.handleRequests()
 
 }
@@ -59,34 +66,73 @@ func (server Server) returnAllNotes(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET")
 	w.Header().Set("Context-Type", "application/json")
-	json.NewEncoder(w).Encode(Notes)
+	notes, err := getAllNotes()
+
+	if err != nil {
+		log.Fatalf("Unable to get all notes/ %v", err)
+	}
+	json.NewEncoder(w).Encode(notes)
+
 }
 
 //use mux to get us single notes
 func (server Server) returnSingleNote(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	//we will need to parse the path parameters
 	vars := mux.Vars(r)
 	// we will need to extract the `id` of the article we
 	// wish to return
-	key := vars["id"]
-
-	for _, note := range Notes {
-		if note.Id == key {
-			json.NewEncoder(w).Encode(note)
-			return
-		}
+	// convert the id type from string to int
+	id, err := strconv.Atoi(vars["id"])
+	//key := vars["id"]
+	if err != nil {
+		log.Fatalf("Unable to convert the string into int.  %v", err)
 	}
+	// call the getUser function with user id to retrieve a single user
+	note, err := getNote(int64(id))
+	if err != nil {
+		log.Fatalf("Unable to get note. %v", err)
+	}
+
+	// send the response
+	json.NewEncoder(w).Encode(note)
+
+	// for _, note := range Notes {
+	// 	if note.Id == key {
+	// 		json.NewEncoder(w).Encode(note)
+	// 		return
+	// 	}
+	// }
 }
 
 func (server Server) createNewNote(w http.ResponseWriter, r *http.Request) {
 	// get the body of our POST request
+	// Allow all origin to handle cors issue
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	// return the string response containing the request body
-	reqBody, _ := ioutil.ReadAll(r.Body)
+
 	var note Note
-	json.Unmarshal(reqBody, &note)
-	Notes = append(Notes, note)
-	json.NewEncoder(w).Encode(note)
+	err := json.NewDecoder(r.Body).Decode(&note)
+	if err != nil {
+		log.Fatalf("Unable to decode the request body.  %v", err)
+	}
+	// call insert user function and pass the note
+	insertID := insertNote(note)
+	// format a response object
+	res := response{
+		ID:      insertID,
+		Message: "User created successfully",
+	}
+	json.NewEncoder(w).Encode(res)
+
+	// reqBody, _ := ioutil.ReadAll(r.Body)
+	// json.Unmarshal(reqBody, &note)
+	// Notes = append(Notes, note)
+	// json.NewEncoder(w).Encode(note)
 }
 
 func (server Server) deleteNote(w http.ResponseWriter, r *http.Request) {
@@ -122,6 +168,184 @@ func (server Server) updateNote(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(Notes)
 
 }
+func insertNote(note Note) int64 {
+	db := createConnection()
+
+	defer db.Close()
+
+	sqlStatement := `INSERT INTO notes (title, description, contents) VALUES ($1, $2, $3) RETURNING id`
+
+	var id int64
+
+	err := db.QueryRow(sqlStatement, note.Title, note.Desc, note.Content).Scan(&id)
+	if err != nil {
+		log.Fatalf("Unable to execute the query. %v", err)
+	}
+	fmt.Printf("Inserted a single record %v", id)
+
+	// return the inserted id
+	return id
+}
+
+//------------------------------Main Request Handler functions--------------------------------//
+
+//run me to make the server work
+func (server Server) handleRequests() {
+
+	createTable()
+	r := mux.NewRouter().StrictSlash(true)
+
+	r.HandleFunc("/api/v1/notes", server.returnAllNotes).Methods("GET")
+	r.HandleFunc("/api/v1/note/{id}", server.returnSingleNote).Methods("GET")
+	r.HandleFunc("/api/v1/note", server.createNewNote).Methods("POST")
+	r.HandleFunc("/api/v1/note/{id}", server.updateNote).Methods("PUT")
+
+	r.HandleFunc("/api/v1/note/{id}", server.deleteNote).Methods("DELETE")
+	r.Handle("/", http.RedirectHandler("/web/", http.StatusPermanentRedirect))
+	r.PathPrefix("/web/").Handler(http.StripPrefix("/web/", http.FileServer(http.Dir("web/"))))
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func getNote(id int64) (Note, error) {
+	// create the postgres db connection
+	db := createConnection()
+
+	// close the db connection
+	defer db.Close()
+
+	// create a user of models.User type
+	var note Note
+
+	// create the select sql query
+	sqlStatement := `SELECT * FROM notes WHERE id=$1`
+
+	// execute the sql statement
+	row := db.QueryRow(sqlStatement, id)
+
+	// unmarshal the row object to user
+	err := row.Scan(&note.Id, &note.Title, &note.Desc, &note.Content)
+
+	switch err {
+	case sql.ErrNoRows:
+		fmt.Println("No rows were returned!")
+		return note, nil
+	case nil:
+		return note, nil
+	default:
+		log.Fatalf("Unable to scan the row. %v", err)
+	}
+
+	// return empty user on error
+	return note, err
+}
+
+// get one user from the DB by its userid
+func getAllNotes() ([]Note, error) {
+	// create the postgres db connection
+	db := createConnection()
+
+	// close the db connection
+	defer db.Close()
+
+	var notes []Note
+
+	// create the select sql query
+	sqlStatement := `SELECT * FROM notes`
+
+	// execute the sql statement
+	rows, err := db.Query(sqlStatement)
+
+	if err != nil {
+		log.Fatalf("Unable to execute the query. %v", err)
+	}
+
+	// close the statement
+	defer rows.Close()
+
+	// iterate over the rows
+	for rows.Next() {
+		var note Note
+
+		// unmarshal the row object to user
+		err = rows.Scan(&note.Id, &note.Title, &note.Desc, &note.Content)
+
+		if err != nil {
+			log.Fatalf("Unable to scan the row. %v", err)
+		}
+
+		// append the user in the users slice
+		notes = append(notes, note)
+
+	}
+
+	// return empty user on error
+	return notes, err
+}
+
+// update user in the DB
+func updateNote(id int64, note Note) int64 {
+
+	// create the postgres db connection
+	db := createConnection()
+
+	// close the db connection
+	defer db.Close()
+
+	// create the update sql query
+	sqlStatement := `UPDATE notes SET title=$2, description=$3, contents=$4 WHERE id=$1`
+
+	// execute the sql statement
+	res, err := db.Exec(sqlStatement, id, note.Title, note.Desc, note.Content)
+
+	if err != nil {
+		log.Fatalf("Unable to execute the query. %v", err)
+	}
+
+	// check how many rows affected
+	rowsAffected, err := res.RowsAffected()
+
+	if err != nil {
+		log.Fatalf("Error while checking the affected rows. %v", err)
+	}
+
+	fmt.Printf("Total rows/record affected %v", rowsAffected)
+
+	return rowsAffected
+}
+
+// delete user in the DB
+func deleteNote(id int64) int64 {
+
+	// create the postgres db connection
+	db := createConnection()
+
+	// close the db connection
+	defer db.Close()
+
+	// create the delete sql query
+	sqlStatement := `DELETE FROM notes WHERE id=$1`
+
+	// execute the sql statement
+	res, err := db.Exec(sqlStatement, id)
+
+	if err != nil {
+		log.Fatalf("Unable to execute the query. %v", err)
+	}
+
+	// check how many rows affected
+	rowsAffected, err := res.RowsAffected()
+
+	if err != nil {
+		log.Fatalf("Error while checking the affected rows. %v", err)
+	}
+
+	fmt.Printf("Total rows/record affected %v", rowsAffected)
+
+	return rowsAffected
+}
+
+//------------------------------SQL Hander functions--------------------------------//
 
 func createConnection() *sql.DB {
 	err := godotenv.Load(".env")
@@ -154,24 +378,4 @@ func createTable() {
 		log.Fatalf("Unable to execute the query. %v", err)
 	}
 	fmt.Printf("%s\n ", res)
-}
-
-//------------------------------SQL Hander functions--------------------------------//
-
-//run me to make the server work
-func (server Server) handleRequests() {
-
-	createTable()
-	r := mux.NewRouter().StrictSlash(true)
-
-	r.HandleFunc("/api/v1/notes", server.returnAllNotes).Methods("GET")
-	r.HandleFunc("/api/v1/note/{id}", server.returnSingleNote).Methods("GET")
-	r.HandleFunc("/api/v1/note", server.createNewNote).Methods("POST")
-	r.HandleFunc("/api/v1/note/{id}", server.updateNote).Methods("PUT")
-
-	r.HandleFunc("/api/v1/note/{id}", server.deleteNote).Methods("DELETE")
-	r.Handle("/", http.RedirectHandler("/web/", http.StatusPermanentRedirect))
-	r.PathPrefix("/web/").Handler(http.StripPrefix("/web/", http.FileServer(http.Dir("web/"))))
-
-	log.Fatal(http.ListenAndServe(":8080", r))
 }
